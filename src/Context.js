513faf9601c5f43b2000fb665d5e31c19ac0c0e1
Haybee2054 <odubena23@gmail.com>
2023-03-14 16:30:19 +0100

    server-update

--- src/Context.js
+++ src/Context.js
@@ -5,7 +5,7 @@ import Peer from 'simple-peer';
 const SocketContext = createContext();
 
 const socket = io('video-server-production.up.railway.app');
-// const socket = io('https://warm-wildwood-81069.herokuapp.com');
+
 
 const ContextProvider = ({ children }) => {
   const [callAccepted, setCallAccepted] = useState(false);
